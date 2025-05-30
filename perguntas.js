292f80a40ab9c7ac4f2f9acca43e6cb1aba77757
Schneider88 <guilherme.schneider.borges@escola.pr.gov.br>
2025-05-30 23:38:53 +0000

    Criando cartões com JS

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Futebol',
+    'Quantos títulos de Champions league o Barcelona FC tem?',
+    'O Barcelona FC tem 5 títulos de Champions league'
+)
+
+criaCartao(
+    'Futebol',
+    'Que time tem mais títulos de Champions league?',
+    'Real Madrid CF'
+)
+
+criaCartao(
+    'Futebol',
+    'Quantos títulos de Brasileirão o Palmeiras tem?',
+    'O Palmeiras tem 12 títulos de Brasileirão'
+)
